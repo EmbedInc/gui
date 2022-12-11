303af072485c79368854f4a127f11fee4ee65a76
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2022-12-11 14:50:01 -0500

    TICKS_MAKE more robust, works over wider ranges.

--- gui_ticks_make.pas
+++ gui_ticks_make.pas
@@ -49,7 +49,7 @@ var
   th: real;                            {standard char cell height}
   space: real;                         {min space required between labels}
   maxsum: real;                        {max sum of adjacent label widths for fit}
-  i: sys_int_machine_t;                {scratch integer}
+  ii: sys_int_machine_t;               {scratch integer}
   r: real;                             {scratch floating point number}
   ran: real;                           {size of values range}
   scale: real;                         {mult factor to convert data values to X}
@@ -58,8 +58,9 @@ var
   seqi: sys_int_machine_t;             {sequence index within curr power of 10}
   sinc: sys_int_machine_t;             {sequence increment within power of 10}
   seqm: sys_int_machine_t;             {sequence increment multiplier}
-  seqmi: sys_int_machine_t;            {SEQM value for first tick}
+  seqmi, seqml: sys_int_machine_t;     {initial and last SEQ multiplier value}
   seq: real;                           {sequence increment}
+  delta: real;                         {delta for being equal to VMIN or VMAX}
   logm: real;                          {sequence power of 10 multiplier}
   tv: real;                            {tick value}
   prevw: real;                         {width of previous label}
@@ -67,44 +68,35 @@ var
   ntick: sys_int_machine_t;            {number of ticks}
   bv, up, ll: vect_2d_t;               {string metrics returned by RENDlib}
   tick_p: gui_tick_p_t;                {pointer to tick descriptor}
-  s: string_var32_t;                   {scratch string for number conversion}
+  minsig: sys_int_machine_t;           {min required significant digits}
+  s, s2: string_var32_t;               {scratch strings for number conversion}
 
 label
   loop_find_maj;
 {
 ****************************************
 *
-*   Local subroutine MAKE_STRING (S, R, LOG10)
+*   Local subroutine MAKE_STRING (S, V)
 *
-*   Set S to the label string to draw for the value R.  LOG10 indicates the
-*   power of 10 within which the label sequence exists.  This is used to decide
-*   which digits to the right of the decimal point, if any must be shown.  A
-*   LOG10 value of 0 is for 1, 1 for 10, -1 for .1, etc.
+*   Set S to the label string to draw for the value V.
 }
 procedure make_string (                {make label string from value}
   in out  s: univ string_var_arg_t;    {the returned string}
-  in      r: real;                     {label numeric value}
-  in      log10: sys_int_machine_t);   {power of 10 sequence is within}
+  in      v: real);                    {label numeric value}
   val_param; internal;
 
 var
-  stat: sys_err_t;                     {completion status code}
+  mn: string_var4_t;                   {factor of 1000 multiplier name}
 
 begin
-  string_f_fp (                        {make string for label}
-    s,                                 {returned string}
-    r,                                 {value to make string of}
-    0,                                 {no fixed field width}
-    0,                                 {no fixed width for exponent}
-    1,                                 {min required significant digits}
-    6,                                 {max digits allowed left of point}
-    max(0, -log10),                    {min required digits right of point}
-    3,                                 {max digits allowed right of point}
-    [ string_ffp_exp_eng_k,            {use engineering notation for exponents}
-      string_ffp_nz_bef_k,             {don't write 0 before point as only char}
-      string_ffp_group_k],             {write digits in groups}
-    stat);
-  if sys_error(stat) then s.len := 0;
+  mn.max := size_char(mn.str);         {init local var string}
+
+  string_f_fp_eng (                    {make string in engineering notation}
+    s,                                 {output string}
+    v,                                 {input value}
+    minsig,                            {min required significant digits}
+    mn);                               {factor of 1000 multiplier name}
+  string_append (s, mn);               {add multiplier name immediately after number}
   end;
 {
 ****************************************
@@ -112,12 +104,14 @@ begin
 *   Start of main routine.
 }
 begin
-  s.max := size_char(s.str);           {init local var string}
+  s.max := size_char(s.str);           {init local var strings}
+  s2.max := size_char(s2.str);
 
   first_p := nil;                      {init to no ticks list created}
   ran := vmax - vmin;                  {set size of values range}
   if ran < 1.0e-30 then return;        {too small a values range to work with ?}
   scale := wid / ran;                  {mult factor from data values to X scale}
+  delta := (vmax - vmin) / 10000.0;    {delta for being equal to VMIN or VMAX}
 
   rend_get.text_parms^ (tp);           {get current text control parameters}
   tw := tp.size * tp.width;            {make standard char cell width}
@@ -125,18 +119,18 @@ begin
   if horiz
     then begin                         {labels will abutt horizontally}
       space := tw * space_k;           {make min space required between labels}
-      i :=                             {can't possibly exceed this many labels}
+      ii :=                            {can't possibly exceed this many labels}
         trunc(wid / (tw * 0.5 + space) + 2.0);
       end
     else begin                         {labels will abutt vertically}
       space := tp.size * tp.lspace;    {make min space required between labels}
-      i :=                             {can't possibly exceed this many labels}
+      ii :=                            {can't possibly exceed this many labels}
         trunc(wid / (th + space) + 2.0);
       lwid := th;                      {"width" of label string}
       end
     ;
-  i := max(10, i);                     {set min number of labels to start with}
-  dmaj := ran / i;                     {init major tick delta to a bit too small}
+  ii := max(10, ii);                   {set min number of labels to start with}
+  dmaj := ran / ii;                    {init major tick delta to a bit too small}
   r := math_log10(dmaj);               {get Log10 of starting major delta}
   log10 := trunc(r + 100.0) - 100;     {find starting base power of 10}
   seqi := 1;                           {init sequence index within this power of 10}
@@ -155,17 +149,49 @@ loop_find_maj:
   seqm := trunc(vmin / seq);           {make initial sequence multiplier}
   tv := seq * seqm;                    {make initial tick value}
   maxsum := (seq * scale - space) * 2.0; {max sum of adjacent label widths for fit}
-
-  while tv < vmin do begin             {loop until tick value up into range}
+  {
+  *   Find the min and max ticks in this series.  SEQMI will be set to the SEQ
+  *   multiplier for the first tick, and SEQML for the last tick.
+  }
+  while tv < (vmin - delta) do begin   {loop until tick value up into range}
     seqm := seqm + 1;                  {go one tick value up}
     tv := seq * seqm;
     end;
-  seqmi := seqm;                       {save info for first tick}
-  prevw := -10.0;                      {init to previous label won't cause squish}
+  seqmi := seqm;                       {save multiplier for first tick}
 
-  while tv <= vmax do begin            {loop for all ticks within range}
+  seqml := seqmi;                      {init multiplier for last tick}
+  while true do begin                  {loop to find last tick that fits range}
+    tv := seq * (seqml + 1);           {make value of next tick}
+    if tv > (vmax + delta) then exit;  {this tick would be past range ?}
+    seqml := seqml + 1;                {no, update last tick multiplier}
+    end;
+
+  ntick := seqml - seqmi + 1;          {number of ticks within this range}
+  {
+  *   Find MINSIG, which is the the minimum number of significant digits
+  *   required to show this progression.  SEQML is the SEQ multiplier for the
+  *   last tick.
+  }
+  tv := (seqml - 1) * seq;             {make value of next to last tick}
+  minsig := 0;                         {init min required significant digits}
+  while true do begin                  {loop until enough sig digits for rel delta}
+    minsig := minsig + 1;              {make significant digits for this try}
+    make_string (s, seq * (seqml - 1)); {make reference label string}
+    make_string (s2, seq * seqml);     {make next label string}
+    if string_equal (s2, s) then next; {not different ?}
+    make_string (s2, seq * (seqml - 2)); {make previous label string}
+    if string_equal (s2, s) then next; {not different ?}
+    exit;                              {prev and next strings different, good enough}
+    end;
+  {
+  *   Loop thru this sequence of ticks to see if the label strings fit.  If not,
+  *   go back and try with next sequence.
+  }
+  prevw := -10.0;                      {init to previous label won't cause squish}
+  for seqm := seqmi to seqml do begin  {loop over each label}
+    tv := seq * seqm;                  {make value of this label}
     if horiz then begin                {label strings in a line horizontally}
-      make_string (s, tv, log10);      {make label string for this tick}
+      make_string (s, tv);             {make label string for this tick}
       rend_get.txbox_txdraw^ (         {measure the label string}
         s.str, s.len,                  {string and string length}
         bv, up, ll);                   {returned string metrics}
@@ -180,18 +206,13 @@ loop_find_maj:
       goto loop_find_maj;              {back to try with this new sequence}
       end;
     prevw := lwid;                     {current width becomes previous width}
-    seqm := seqm + 1;                  {advance to next tick value in this sequence}
-    tv := seq * seqm;
     end;                               {back to check out this new tick value}
-
-  ntick := seqm - seqmi;               {make number of ticks within range}
 {
 *   A sequence was found for which all the labels fit next to each other.  NTICK
-*   is the number of ticks.  SEQMI is the SEQM value for the first tick.
+*   is the number of ticks.  SEQMI to SEQML are the SEQ multipliers for the
+*   first and last ticks.
 }
-  seqm := seqmi;
-
-  for i := 1 to ntick do begin         {once for each tick descriptor to create}
+  for seqm := seqmi to seqml do begin  {once for each tick descriptor to create}
     tv := seq * seqm;                  {make data value of this tick}
 
     util_mem_grab (                    {allocate memory for new tick descriptor}
@@ -202,8 +223,6 @@ loop_find_maj:
     tick_p^.val := tv;                 {set tick data value}
     tick_p^.level := 0;                {this is a major tick}
     tick_p^.lab.max := size_char(tick_p^.lab.str);
-    make_string (tick_p^.lab, tv, log10); {set label string for this tick}
-
-    seqm := seqm + 1;                  {advance to next tick in sequence}
+    make_string (tick_p^.lab, tv);     {set label string for this tick}
     end;                               {back to create next tick descriptor}
   end;
